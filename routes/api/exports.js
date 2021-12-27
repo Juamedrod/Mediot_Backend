c6653c9aaf7a5bd56b508613e8f1f8b84a8736c1
JM_Medina <juamedrod@hotmail.es>
2021-12-27 13:21:19 +0100

    Heroku filesystem hotfix

--- routes/api/exports.js
+++ routes/api/exports.js
@@ -14,22 +14,22 @@ router.get('/:dId/:var', async (req, res) => {
         console.log(req.query.limit);
         const snapshot = await Data.find({ dId: req.params.dId }).sort({ _id: -1 }).limit(parseInt(req.query.limit));
         let csv = await createCSV(snapshot, ['_id', 'dId', 'iat', `variables.${req.params.var}`], true);
-        console.log('He llegado aquí');
+        /* console.log('He llegado aquí'); */
         const writeStream = fs.createWriteStream(`./public/csv/${req.params.dId}${random}.csv`, { flags: 'w' });
-        console.log('he creado el fichero');
+        /* console.log('he creado el fichero'); */
         writeStream.write(csv);
         writeStream.end();
         res.json({ url: `csv/${req.params.dId}${random}.csv` });
-        /*  new setTimeout(async function () {
-             try {
-                 fs.rm(`./public/csv/${req.params.dId}${random}.csv`, (error) => {
-                     if (error) throw error;
-                 });
-             } catch (error) {
-                 console.log({ error: error.message });
-                 res.json({ error: error.message });
-             }
-         }, 20000); */
+        new setTimeout(async function () {
+            try {
+                fs.rm(`./public/csv/${req.params.dId}${random}.csv`, (error) => {
+                    if (error) throw error;
+                });
+            } catch (error) {
+                console.log({ error: error.message });
+                res.json({ error: error.message });
+            }
+        }, 20000);
     } catch (error) {
         console.log({ error: error.message });
         res.json({ error: error.message });
